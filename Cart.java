3bb3efeee8f7c594e071761637d26331f86cc035
Chandler Mattila <45371206+csmattila@users.noreply.github.com>
2021-02-13 22:52:56 -0600

    Update Cart.java

--- Cart.java
+++ Cart.java
@@ -1,12 +1,33 @@
 package warehouseproject;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 class Cart implements Serializable{
     private static final long serialVersionUID = 1L;
+    private ArrayList<Product> products = new ArrayList<Product>();
     
-    public void addToCart(Product product){
+    public boolean addToCart(Product product, int quantity){
         
+        //Create a copy of the product object using the copy constructor
+        Product copiedProduct = new Product(product);
+        copiedProduct.setQuantity(quantity);
+        return products.add(copiedProduct);
     }
+
+    public Iterator getCartContents(){
+        return products.iterator();
+    }
+    @Override
+    public String toString(){
+        String productString = "";
+        for(Product p : products){
+            productString += "\n" + p;
+        }
+        return productString;
+    }
+
+    
     
 }
